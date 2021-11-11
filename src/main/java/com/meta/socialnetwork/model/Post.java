cf0fd4b567583ea717fbb657e1b1747a28e35189
Your Name <dhnam.2020@gmail.com>
2021-11-11 09:20:12 +0700

    fix deploy

--- src/main/java/com/meta/socialnetwork/model/Post.java
+++ src/main/java/com/meta/socialnetwork/model/Post.java
@@ -9,12 +9,13 @@ import org.hibernate.Hibernate;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 @Entity
-@Getter
-@Setter
+//@Getter
+//@Setter
 @Table(name = "posts")
 @AllArgsConstructor
 @NoArgsConstructor
@@ -37,11 +38,97 @@ public class Post {
     @JoinColumn(name = "user_id")
     private User user;
 
-    @OneToMany(mappedBy = "post")
-    private List<Comment> commentList;
+    @OneToMany(fetch = FetchType.LAZY,mappedBy = "post")
+    private List<Comment> commentList = null;
 
-    @OneToMany(mappedBy = "posts")
-    private List<Like> likeList;
+    @OneToMany(fetch = FetchType.LAZY,mappedBy = "posts")
+    private List<Like> likeList = null;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public LocalDate getCreated_date() {
+        return created_date;
+    }
+
+    public void setCreated_date(LocalDate created_date) {
+        this.created_date = created_date;
+    }
+
+    public LocalDate getModified_date() {
+        return modified_date;
+    }
+
+    public void setModified_date(LocalDate modified_date) {
+        this.modified_date = modified_date;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Comment> getCommentList() {
+        if(commentList == null) {
+            commentList = new ArrayList<Comment>();
+        }
+        return commentList;
+    }
+
+    public void setCommentList(List<Comment> commentList) {
+        this.commentList = commentList;
+    }
+
+    public List<Like> getLikeList() {
+        if(likeList==null){
+            likeList = new ArrayList<>();
+        }
+        return likeList;
+    }
+
+    public void setLikeList(List<Like> likeList) {
+        this.likeList = likeList;
+    }
+
+    public Long getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(Long commentId) {
+        this.commentId = commentId;
+    }
 
     @Transient
     private Long commentId;
